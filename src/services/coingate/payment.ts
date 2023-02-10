01d7f00e6117e46ce7af7dcf23d740ba5d86a744
Jeus Bausa <jeusdbausa@gmail.com>
2023-02-10 21:37:20 +0800

    changes

--- src/services/coingate/payment.ts
+++ src/services/coingate/payment.ts
@@ -5,15 +5,18 @@ import { generateOrderId } from "../../utils/common";
 import { env } from "../../utils/env";
 
 const createOrder = async (body: ICreateOrderSchemaBody): AxiosPromise => {
-    const success_url = `${env.COINGATE_SUCCESS_PAGE}?order_id=${body.order_id}`;
-    const cancel_url = `${env.COINGATE_CANCEL_PAGE}?order_id=${body.order_id}`;
+
+    const order_id = await generateOrderId();
+
+    const success_url = `${env.COINGATE_SUCCESS_PAGE}?order_id=${order_id}`;
+    const cancel_url = `${env.COINGATE_CANCEL_PAGE}?order_id=${order_id}`;
 
     return coingate.post("/orders", {
         ...body,
         success_url,
         cancel_url,
         callback_url: env.COINGATE_API_CALLBACK,
-        order_id: await generateOrderId(),
+        order_id,
     });
 };
 
